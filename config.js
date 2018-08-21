6b87592ebe8d1a9668c529168e3b568d214db6b9
zakir shaikh <zakrshkh07@gmail.com>
2018-08-21 19:33:24 +0530

    user changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
   user: 'postgres', //env var: PGUSER 
   database: 'pos', //env var: PGDATABASE 
-  password: 'zeartech', //env var: PGPASSWORD 
+  password: 'zakrshkh', //env var: PGPASSWORD 
   host: '127.0.0.1', // Server hosting the postgres database 
   port: 5432, //env var: PGPORT 
   max: 10, // max number of clients in the pool 
