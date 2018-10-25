f37a6a5b755650c816a4d26d6c1dda6c5bd6dae7
Raees Shaikh <raees@3commastechnologies.com>
2018-10-25 13:47:49 +0530

    category service update

--- routes/category.js
+++ routes/category.js
@@ -7,28 +7,6 @@ var config = require('../config.js');
 
 var pool = new pg.Pool(config);
 
-router.get('/', oauth.authorise(), (req, res, next) => {
-  const results = [];
-  pool.connect(function(err, client, done){
-    if(err) {
-      done();
-      // pg.end();
-      console.log("the error is"+err);
-      return res.status(500).json({success: false, data: err});
-    }
-    const query = client.query("SELECT * FROM CATEGORY_MASTER where ctm_status = 0 order by ctm_id desc");
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    query.on('end', () => {
-      done();
-      // pg.end();
-      return res.json(results);
-    });
-  done(err);
-  });
-});
-
 router.get('/:ctmId', oauth.authorise(), (req, res, next) => {
   const results = [];
   const id = req.params.ctmId;
@@ -53,7 +31,7 @@ router.get('/:ctmId', oauth.authorise(), (req, res, next) => {
   });
 });
 
-/*router.post('/add', oauth.authorise(), (req, res, next) => {
+router.post('/checkname', oauth.authorise(), (req, res, next) => {
   const results = [];
   pool.connect(function(err, client, done){
     if(err) {
@@ -62,10 +40,15 @@ router.get('/:ctmId', oauth.authorise(), (req, res, next) => {
       console.log("the error is"+err);
       return res.status(500).json({success: false, data: err});
     }
-    // SQL Query > Insert Data
-    client.query('INSERT INTO category_master(ctm_type, ctm_username, ctm_status) values($1,$2,0)',[req.body.ctm_type,req.body.ctm_username]);
+
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM category_master');
+    const strqry =  "SELECT * "+
+                    "from category_master ctm"+
+                    "inner join restaurant_master srm on ctm.ctm_srm_id=srm.srm_id "+
+                    "where ctm_status=0 "+
+                    "and ctm_srm_id = $1 "+
+                    "and LOWER(ctm_type) LIKE LOWER($2) ";
+    const query = client.query(strqry,[req.body.ctm_srm_id,req.body.ctm_type]);
     query.on('row', (row) => {
       results.push(row);
     });
@@ -74,82 +57,98 @@ router.get('/:ctmId', oauth.authorise(), (req, res, next) => {
       // pg.end();
       return res.json(results);
     });
-  done(err);
-  });
-});*/
-
-router.post('/add', oauth.authorise(), (req, res, next) => {
-  const results = [];
-  pool.connect(function(err, client, done){
-    if(err) {
-      done();
-      // pg.end();
-      console.log("the error is"+err);
-      return res.status(500).json({success: false, data: err});
-    }
-
-    var singleInsert = 'INSERT INTO category_master(ctm_type,ctm_username,ctm_status) values($1,$2,0) RETURNING *',
-        params = [req.body.ctm_type,req.body.ctm_username]
-    client.query(singleInsert, params, function (error, result) {
-        results.push(result.rows[0]); // Will contain your inserted rows
-        done();
-        return res.json(results);
-    });
-
     done(err);
   });
 });
 
-/*router.post('/edit/:ctmId', oauth.authorise(), (req, res, next) => {
+router.post('/add', oauth.authorise(), (req, res, next) => {
   const results = [];
-  const id = req.params.ctmId;
-  pool.connect(function(err, client, done){
-    if(err) {
-      done();
-      // pg.end();
-      console.log("the error is"+err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Insert Data
-    client.query('UPDATE category_master SET ctm_type=$1, ctm_updated_at=now() where ctm_id=$2',[req.body.ctm_type,id]);
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM category_master');
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    query.on('end', () => {
-      done();
-      // pg.end();
-      return res.json(results);
-    });
-  done(err);
+
+  var Storage = multer.diskStorage({
+      destination: function (req, file, callback) {
+          // callback(null, "./images");
+            callback(null, "../nginx/html/images");
+      },
+      filename: function (req, file, callback) {
+          var fi = file.fieldname + "_" + Date.now() + "_" + file.originalname;
+          filenamestore = "../images/"+fi;
+          callback(null, fi);
+      }
   });
-});*/
+
+  var upload = multer({ storage: Storage }).array("ctm_image"); 
+
+  upload(req, res, function (err) { 
+    if (err) { 
+        return res.end("Something went wrong!"+err); 
+    } 
+
+    pool.connect(function(err, client, done){
+      if(err) {
+        done();
+        // pg.end();
+        console.log("the error is"+err);
+        return res.status(500).json({success: false, data: err});
+      }
+
+      var singleInsert = 'INSERT INTO category_master(ctm_type,ctm_image,ctm_srm_id) values($1,$2,$3) RETURNING *',
+          params = [req.body.ctm_type,filenamestore,req.body.ctm_srm_id]
+      client.query(singleInsert, params, function (error, result) {
+          results.push(result.rows[0]); // Will contain your inserted rows
+          done();
+          return res.json(results);
+      });
+
+    done(err);
+    });
+  }); 
+});
 
 router.post('/edit/:productId', oauth.authorise(), (req, res, next) => {
   const results = [];
   const id = req.params.productId;
   /*const product = req.body.category;*/
-  pool.connect(function(err, client, done){
-    if(err) {
-      done();
-      // pg.end();
-      console.log("the error is"+err);
-      return res.status(500).json({success: false, data: err});
-    }
-    client.query('BEGIN;');
-    
-    var singleInsert = 'update category_master set ctm_type=$1, ctm_updated_at=now() where ctm_id=$2 RETURNING *',
-        params = [req.body.ctm_type,id];
-    client.query(singleInsert, params, function (error, result) {
-        results.push(result.rows[0]); // Will contain your inserted rows
-        
-        client.query('COMMIT;');
-        done();
-        return res.json(results);
-    });
 
-    done(err);
+  var Storage = multer.diskStorage({
+      destination: function (req, file, callback) {
+          // callback(null, "./images");
+            callback(null, "../nginx/html/images");
+      },
+      filename: function (req, file, callback) {
+          var fi = file.fieldname + "_" + Date.now() + "_" + file.originalname;
+          filenamestore = "../images/"+fi;
+          callback(null, fi);
+      }
+  });
+
+  var upload = multer({ storage: Storage }).array("ctm_image"); 
+
+  upload(req, res, function (err) { 
+    if (err) { 
+        return res.end("Something went wrong!"+err); 
+    } 
+
+    pool.connect(function(err, client, done){
+      if(err) {
+        done();
+        // pg.end();
+        console.log("the error is"+err);
+        return res.status(500).json({success: false, data: err});
+      }
+      client.query('BEGIN;');
+      
+      var singleInsert = 'update category_master set ctm_type=$1, ctm_image=$2, ctm_updated_at=now() where ctm_id=$3 RETURNING *',
+          params = [req.body.ctm_type,filenamestore,id];
+      client.query(singleInsert, params, function (error, result) {
+          results.push(result.rows[0]); // Will contain your inserted rows
+          
+          client.query('COMMIT;');
+          done();
+          return res.json(results);
+      });
+
+      done(err);
+    });
   });
 });
 
@@ -192,11 +191,13 @@ router.post('/category/total', oauth.authorise(), (req, res, next) => {
 
     console.log(str);
     const strqry =  "SELECT count(ctm_id) as total "+
-                    "from category_master "+
+                    "from category_master ctm"+
+                    "inner join restaurant_master srm on ctm.ctm_srm_id=srm.srm_id "+
                     "where ctm_status=0 "+
-                    "and LOWER(ctm_type) LIKE LOWER($1);";
+                    "and ctm_srm_id = $1 "+
+                    "and LOWER(ctm_type) LIKE LOWER($2);";
 
-    const query = client.query(strqry,[str]);
+    const query = client.query(strqry,[req.body.ctm_srm_id,str]);
     query.on('row', (row) => {
       results.push(row);
     });
@@ -221,13 +222,15 @@ router.post('/category/limit', oauth.authorise(), (req, res, next) => {
     const str = "%"+req.body.search+"%";
     // SQL Query > Select Data
 
-    const strqry =  "SELECT ctm.ctm_id, ctm.ctm_type "+
-                    "FROM category_master ctm "+
-                    "where ctm.ctm_status = 0 "+
-                    "and LOWER(ctm_type) LIKE LOWER($1) "+
-                    "order by ctm.ctm_id desc LIMIT $2 OFFSET $3";
+    const strqry =  "SELECT * "+
+                    "from category_master ctm"+
+                    "inner join restaurant_master srm on ctm.ctm_srm_id=srm.srm_id "+
+                    "where ctm_status=0 "+
+                    "and ctm_srm_id = $1 "+
+                    "and LOWER(ctm_type) LIKE LOWER($2) "+
+                    "order by ctm.ctm_id desc LIMIT $3 OFFSET $4";
 
-    const query = client.query(strqry,[ str, req.body.number, req.body.begin]);
+    const query = client.query(strqry,[req.body.ctm_srm_id, str, req.body.number, req.body.begin]);
     query.on('row', (row) => {
       results.push(row);
     });
@@ -252,13 +255,15 @@ router.post('/typeahead/search', oauth.authorise(), (req, res, next) => {
     const str = "%"+req.body.search+"%";
     // SQL Query > Select Data
 
-    const strqry =  "SELECT ctm.ctm_id, ctm.ctm_type "+
-                    "FROM category_master ctm "+
-                    "where ctm.ctm_status = 0 "+
-                    "and LOWER(ctm_type) LIKE LOWER($1) "+
+    const strqry =  "SELECT * "+
+                    "from category_master ctm"+
+                    "inner join restaurant_master srm on ctm.ctm_srm_id=srm.srm_id "+
+                    "where ctm_status=0 "+
+                    "and ctm_srm_id = $1 "+
+                    "and LOWER(ctm_type) LIKE LOWER($2) "+
                     "order by ctm.ctm_id desc LIMIT 10";
 
-    const query = client.query(strqry,[ str]);
+    const query = client.query(strqry,[req.body.ctm_srm_id, str]);
     query.on('row', (row) => {
       results.push(row);
     });
